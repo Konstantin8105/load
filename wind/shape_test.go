7474dd0b70d693b9d7e3a2997b9924ed84149416
Konstantin8105 <Konstantin8105@gmail.com>
2020-06-26 00:46:51 +0300

    shape: add test

---
+++ wind/shape_test.go
@@ -0,0 +1,11 @@
+package wind
+
+import "fmt"
+
+func ExampleRectangle() {
+	Rectangle(ZoneA, RegionII, LogDecriment15, 5.38, 7.32, 18.965, []float64{1.393})
+
+	fmt.Println(">>>>>>>> Wsum = ", 790, 630, 390, 630, 390)
+
+	// Output:
+}
